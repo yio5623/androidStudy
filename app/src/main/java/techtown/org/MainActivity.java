31bba5df4952f5985911bdc6599843a0fed04783
MinkyuJoo5623 <yioyio5623@gmail.com>
2020-09-01 19:53:24 +0900

    Test Git

---
+++ app/src/main/java/techtown/org/MainActivity.java
@@ -0,0 +1,15 @@
+package techtown.org;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int x=10;
+    }
+}
