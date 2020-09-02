48b29fda6bf068c3b0679e09b18c435602371015
MinkyuJoo5623 <yioyio5623@gmail.com>
2020-09-02 19:13:47 +0900

    add button, Uri

--- app/src/main/java/techtown/org/MainActivity.java
+++ app/src/main/java/techtown/org/MainActivity.java
@@ -1,8 +1,12 @@
 package techtown.org;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onButton1Clicked(View v){
+        Toast.makeText(getApplicationContext(), "버튼이 눌림", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+
 }
